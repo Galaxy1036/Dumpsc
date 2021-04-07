63681237d75bd00402f59ed29ede9f3699ba478f
Galaxy1036 <24322981+Galaxy1036@users.noreply.github.com>
2021-04-07 09:44:53 +0200

    Little rewrite
    Add zstd support and refactor some old code

--- Dumpsc.py
+++ Dumpsc.py
@@ -3,9 +3,9 @@ import lzma
 import lzham
 import struct
 import argparse
+import zstandard
 
 from PIL import Image
-from math import floor
 
 
 def convert_pixel(pixel, type):
@@ -27,11 +27,11 @@ def convert_pixel(pixel, type):
         pixel, = struct.unpack("<H", pixel)
         return (((pixel >> 11) & 0x1F) << 3, ((pixel >> 5) & 0x3F) << 2, (pixel & 0x1F) << 3)
     elif type == 6:
-        #LA88 = Luminance Alpha 88
+        # LA88 = Luminance Alpha 88
         pixel, = struct.unpack("<H", pixel)
         return (pixel >> 8), (pixel >> 8), (pixel >> 8), (pixel & 0xFF)
     elif type == 10:
-        #L8 = Luminance8
+        # L8 = Luminance8
         pixel, = struct.unpack("<B", pixel)
         return pixel, pixel, pixel
     else:
@@ -42,37 +42,49 @@ def process_sc(baseName, data, path, decompress):
     if decompress:
         if data[:2] == b'SC':
             # Skip the header if there's any
+            version = int.from_bytes(data[2: 6], 'big')
             hash_length = int.from_bytes(data[6: 10], 'big')
             data = data[10 + hash_length:]
 
-        if data[:4] == b'SCLZ':
-            print('[*] Detected LZHAM compression !')
+        if version in (1, 3):  # Version 2 in HDP does not use any compression
+            if data[:4] == b'SCLZ':
+                print('[*] Detected LZHAM compression !')
 
-            dict_size = int.from_bytes(data[4:5], 'big')
-            uncompressed_size = int.from_bytes(data[5:9], 'little')
+                dict_size = int.from_bytes(data[4:5], 'big')
+                uncompressed_size = int.from_bytes(data[5:9], 'little')
 
-            try:
-                decompressed = lzham.decompress(data[9:], uncompressed_size, {'dict_size_log2': dict_size})
+                try:
+                    decompressed = lzham.decompress(data[9:], uncompressed_size, {'dict_size_log2': dict_size})
 
-            except Exception as exception:
-                print('Cannot decompress {} !'.format(baseName))
-                return
+                except Exception as exception:
+                    print('Cannot decompress {} !'.format(baseName))
+                    return
+
+            elif data[:4] == bytes.fromhex('28 B5 2F FD'):
+                print('[*] Detected Zstandard compression !')
+
+                try:
+                    decompressed = zstandard.decompress(data)
+
+                except Exception as exception:
+                    print('Cannot decompress {} !'.format(baseName))
+                    return
+
+            else:
+                print('[*] Detected LZMA compression !')
+                data = data[0:9] + (b'\x00' * 4) + data[9:]
+
+                try:
+                    decompressed = lzma.LZMADecompressor().decompress(data)
+
+                except Exception as exception:
+                    print('Cannot decompress {} !'.format(baseName))
+                    return
+
+            print('[*] Successfully decompressed {} !'.format(baseName))
 
         else:
-            print('[*] Detected LZMA compression !')
-            data = data[0:9] + (b'\x00' * 4) + data[9:]
-
-            try:
-                decompressed = lzma.LZMADecompressor().decompress(data)
-
-            except Exception as exception:
-                print('Cannot decompress {} !'.format(baseName))
-                return
-
-        print('[*] Successfully decompressed {} !'.format(baseName))
-
-    else:
-        decompressed = data
+            decompressed = data
 
     i = 0
     picCount = 0
@@ -83,9 +95,10 @@ def process_sc(baseName, data, path, decompress):
         width, = struct.unpack('<H', decompressed[i + 6:i + 8])
         height, = struct.unpack('<H', decompressed[i + 8:i + 10])
         i += 10
-        if subType == 0 or subType == 1:
+
+        if subType in (0, 1):
             pixelSize = 4
-        elif subType == 2 or subType == 3 or subType == 4 or subType == 6:
+        elif subType in (2, 3, 4, 6):
             pixelSize = 2
         elif subType == 10:
             pixelSize = 1
@@ -97,17 +110,21 @@ def process_sc(baseName, data, path, decompress):
 
         img = Image.new("RGBA", (width, height))
         pixels = []
+
         for y in range(height):
             for x in range(width):
                 pixels.append(convert_pixel(decompressed[i:i + pixelSize], subType))
                 i += pixelSize
+
         img.putdata(pixels)
+
         if fileType == 29 or fileType == 28 or fileType == 27:
             imgl = img.load()
             iSrcPix = 0
-            for l in range(floor(height / 32)):  # block of 32 lines
+
+            for l in range(height // 32):  # block of 32 lines
                 # normal 32-pixels blocks
-                for k in range(floor(width / 32)):  # 32-pixels blocks in a line
+                for k in range(width // 32):  # 32-pixels blocks in a line
                     for j in range(32):  # line in a multi line block
                         for h in range(32):  # pixels in a block
                             imgl[h + (k * 32), j + (l * 32)] = pixels[iSrcPix]
@@ -118,7 +135,7 @@ def process_sc(baseName, data, path, decompress):
                         imgl[h + (width - (width % 32)), j + (l * 32)] = pixels[iSrcPix]
                         iSrcPix += 1
             # final lines
-            for k in range(floor(width / 32)):  # 32-pixels blocks in a line
+            for k in range(width // 32):  # 32-pixels blocks in a line
                 for j in range(height % 32):  # line in a multi line block
                     for h in range(32):  # pixels in a 32-pixels-block
                         imgl[h + (k * 32), j + (height - (height % 32))] = pixels[iSrcPix]
@@ -156,4 +173,4 @@ if __name__ == "__main__":
                 process_sc(basename, f.read(), path, args.decompress)
 
         else:
-            print('[*] Only tex.sc are supported !'.format(file))
+            print('[*] Only tex.sc are supported !')
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # Dumpsc
-A tool for dump sc content to png file forked on cr-sc-dump by 132456adcdef
+**Dumpsc** is an adapted version of [cr-sc-dump](https://github.com/123456abcdef/cr-sc-dump). This version aim to support all kind of **_tex.sc** files from different Supercell games.
+
+
+
+## Usage
+
+If your file is compressed run the following command:
+
+> python dumpsc.py <your_tex_sc_file> -d
+
+else simply use:
+
+> python dumpsc.py <your_tex_sc_file
+
+
+
+## Dependencies
+
+To install **Dumpsc** dependencies run the following command:
+
+> python -m pip install -r requirements.txt
